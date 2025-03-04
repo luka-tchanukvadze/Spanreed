7ac3173e7f62ef69e77b5f99469ebea51b51699b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-04 20:54:55 +0400

    adding signup functionality

---
+++ frontend/src/hooks/useSignup.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+
+type SignUpInputs = {
+  fullName: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+};
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const signup = async (inputs: SignUpInputs) => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) throw new Error(data.error);
+
+      setAuthUser(data);
+    } catch (error: any) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+};
+
+export default useSignup;
