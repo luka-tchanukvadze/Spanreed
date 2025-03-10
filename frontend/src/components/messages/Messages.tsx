91c73d6cd5f0eedc7d14ca98a21c53aba05c00f4
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-10 21:46:27 +0400

    get messages

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,13 +1,16 @@
 import { DUMMY_MESSAGES } from "../../dummy_data/dummy";
+import useGetConversations from "../../hooks/useGetConversations";
+import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 
 const Messages = () => {
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			{DUMMY_MESSAGES.map((message) => (
-				<Message key={message.id} message={message} />
-			))}
-		</div>
-	);
+  const { loading, messages } = useGetMessages();
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </div>
+  );
 };
 export default Messages;
