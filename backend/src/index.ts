37e1e31eb8a3479c1ca0ed0aa3c0a961be065f5d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-14 21:25:26 +0400

    finished back end side

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,10 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
 import dotenv from 'dotenv'
+import {app, server} from './socket/socket.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 5001
-
-const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
@@ -18,7 +17,7 @@ app.use('/api/auth', authRoutes)
 
 app.use('/api/messages', messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(
     `Server is running on port: ${PORT}`
   )
