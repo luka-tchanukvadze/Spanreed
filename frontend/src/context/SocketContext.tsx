bdc217a7cf3788ffd3a41db00fa91afd1c750067
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-14 21:32:05 +0400

    getting ready for socket context

---
+++ frontend/src/context/SocketContext.tsx
@@ -0,0 +1,10 @@
+import {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  ReactNode,
+  useRef,
+} from "react";
+import { useAuthContext } from "./AuthContext";
+import io, { Socket } from "socket.io-client";
