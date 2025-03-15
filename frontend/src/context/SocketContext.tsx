700068b7e27a4be06eae4b5affb4dcd8f0627a1d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-15 08:17:51 +0400

    creating socketcontext

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -13,3 +13,20 @@ interface ISocketContext {
   socket: Socket | null;
   onlineUsers: string[];
 }
+
+const SocketContext = createContext<ISocketContext | undefined>(undefined);
+
+const SocketContextProvider = ({ children }: { children: ReactNode }) => {
+  const socketRef = useRef<Socket | null>(null);
+
+  const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
+  const { authUser, isLoading } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser && !isLoading) {
+      const socket = io(socketURL);
+    }
+  }, []);
+};
+
+export default SocketContextProvider;
