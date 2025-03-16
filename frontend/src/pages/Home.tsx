1cd0a1fab1d9db7759beb8cc9870ef167d127b9d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-16 21:44:12 +0400

    background filter

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex h-[80vh] w-full md:max-w-screen-md md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0">
+    <div className="flex h-[80vh] w-full md:max-w-screen-md md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter bg-opacity-0 backdrop-blur-md">
       <Sidebar />
       <MessageContainer />
     </div>
