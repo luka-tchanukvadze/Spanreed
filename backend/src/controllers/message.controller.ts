ead42be4ef6e9fe5f0a6af0fa4839459cec73608
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-16 17:30:58 +0400

    sending messages real time backend

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prisma.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req: Request, res: Response): Promise<any> => {
   try {
@@ -50,6 +51,12 @@ export const sendMessage = async (req: Request, res: Response): Promise<any> => 
     }
 
     // Socket io will go here
+    const receiverSocketId = getReceiverSocketId(receiverId)
+
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit('newMessage', newMesage)
+    }
+
     res.status(201).json(newMesage)
   } catch (error: any) {
     console.error('Error in sendMessage: ', error.message)
