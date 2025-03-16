5617567616b568b2ca3a940f00eac38d37430feb
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-16 21:52:26 +0400

    fixing tailwind config for production

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,12 @@ import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "backdrop-blur",
+    "backdrop-blur-sm",
+    "backdrop-blur-md",
+    "backdrop-blur-lg",
+  ],
   theme: {
     extend: {},
   },
